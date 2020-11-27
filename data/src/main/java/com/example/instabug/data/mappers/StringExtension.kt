e8191b013af65ffe9fe8d257d53bed49dfd7dcc3
hebahassan <heba.hassan.elnashar@gmail.com>
2020-11-27 23:11:39 +0200

    remove unused packages

--- data/src/main/java/com/example/instabug/data/mappers/StringExtension.kt
+++ data/src/main/java/com/example/instabug/data/mappers/StringExtension.kt
@@ -2,7 +2,7 @@ package com.example.instabug.data.mappers
 
 internal fun String.ignoreSpecialCharacters(): String {
     val regex = "[^A-Za-z ]".toRegex()
-    return regex.replace(this, "")
+    return regex.replace(this, " ")
 }
 
 internal fun String.toStringList(): List<String> = this.split(" ")
